d7f7219aaec2131a60d31d6410bc83d8f7f989db
rickyye14 <ricky.ye14@gmail.com>
2014-05-15 19:48:56 -0500

    backup

---
+++ java/ContainerWithMostWater.java
@@ -0,0 +1,17 @@
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length - 1;
+        int maxArea = 0;
+        while (start < end) {
+            maxArea = Math.max(maxArea,
+                    Math.min(height[start], height[end]) * (end - start));
+            if (height[start] < height[end]) {
+                ++start;
+            } else {
+                --end;
+            }
+        }
+        return maxArea;
+    }
+}
