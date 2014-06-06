c0b2838a5a8fcc0118b8cd7c0eac835b37bf29af
rickyye14 <ricky.ye14@gmail.com>
2014-06-06 10:49:00 -0500

    level order traversal

---
+++ java/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class PopulatingNextRightPointersInEachNode {
+    public void connect(TreeLinkNode root) {
+        if (root == null) {
+            return;
+        }
+        ArrayList<TreeLinkNode> current = null;
+        ArrayList<TreeLinkNode> post = new ArrayList<TreeLinkNode>();
+        post.add(root);
+        while (post.size() != 0) {
+            current = post;
+            post = new ArrayList<TreeLinkNode>();
+            int n = current.size();
+            for (int i = 0; i < n - 1; ++i) {
+                TreeLinkNode node = current.get(i);
+                node.next = current.get(i + 1);
+            }
+            for (int i = 0; i < n; ++i) {
+                TreeLinkNode node = current.get(i);
+                if (node.left != null) {
+                    post.add(node.left);
+                }
+                if (node.right != null) {
+                    post.add(node.right);
+                }
+            }
+        }
+    }
+}
