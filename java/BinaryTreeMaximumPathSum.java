b6701ac15b990a6ffd11ce6c5bc71b81d20f4e1b
rickyye14 <ricky.ye14@gmail.com>
2014-06-24 15:51:19 -0500

    recursion, dynamic programming

---
+++ java/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,22 @@
+public class BinaryTreeMaximumPathSum {
+    private int maxSum;
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        maxSum = Integer.MIN_VALUE;
+        searchMax(root);
+        return maxSum;
+    }
+
+    private int searchMax(TreeNode curr) {
+        if (curr == null) {
+            return 0;
+        }
+        int leftMax = Math.max(0, searchMax(curr.left));
+        int rightMax = ,ath.max(0, searchMax(curr.right));
+        maxSum = Math.max(maxSum, curr.val + leftMax + rightMax);
+        return curr.val + Math.max(0, Math.max(leftMax, rightMax));
+    }
+}
