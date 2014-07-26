e0c9d5f084ab3fcfbb0e3e997cb918ba64a4909b
rickyye14 <ricky.ye14@gmail.com>
2014-07-26 17:18:19 -0500

    implementation

---
+++ cc/zigzag_conversion.cc
@@ -0,0 +1,47 @@
+#include "base_include.h"
+
+class ZigZagCoversion {
+private:
+    struct CharPosition {
+        char ch;
+        int x;
+        int y;
+
+        CharPosition() {}
+
+        CharPosition(int ch, int x, int y):
+            ch(ch), x(x), y(y) {}
+    };
+
+public:
+    string convert(string s, int nRows) {
+        int n = s.size();
+        if (0 == n) {
+            return s;
+        }
+        if (1 == nRows) {
+            return s;
+        }
+        CharPosition cp[n];
+        for (int index = 0, x = 0, y = 0; index < n; ) {
+            for (int i = 0 ; index < n && i < nRows - 1; ++i, ++index) {
+                cp[index] = CharPosition(s[index], x, y);
+                ++x;
+            }
+            if (index == n) {
+                break;
+            }
+            for (int i = 0; index < n && i < nRows - 1; ++i, ++index) {
+                cp[index] = CharPosition(s[index], x, y);
+                --x;
+                ++y;
+            }
+        }
+        sort(cp, cp + n, [](const CharPosition &cp1, const CharPosition &cp2){ return cp1.x != cp2.x ? cp1.x < cp2.x : cp1.y < cp2.y; });
+        string res;
+        for (int i = 0; i < n; ++i) {
+            res += cp[i].ch;
+        }
+        return res;
+    }
+};
