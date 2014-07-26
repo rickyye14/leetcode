700014cd734998299cbd4edd035b8f4fab03be9b
rickyye14 <ricky.ye14@gmail.com>
2014-07-26 17:23:25 -0500

    implementation

---
+++ cc/reverse_integer.cc
@@ -0,0 +1,11 @@
+class ReverseInteger {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return res;
+    }
+};
