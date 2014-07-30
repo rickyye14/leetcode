d7620104160894f2e04b1a92b3c9afa6b22d8bde
rickyye14 <ricky.ye14@gmail.com>
2014-07-29 20:59:05 -0500

    update

---
+++ cc/base_include.h
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <limits>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+
+#include <cmath>
+
+using std::sort;
+using std::numeric_limits;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::unordered_set;
+using std::unordered_map;
+
+using std::min;
+using std::max;
+using std::ceil;
+using std::floor;
