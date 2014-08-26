eb8bded27f7878561729d2af6767a6a6f0bca70f
rickyye14 <ricky.ye14@gmail.com>
2014-08-26 09:22:12 -0500

    update

--- cc/reverse_nodes_in_k_group.cc
+++ cc/reverse_nodes_in_k_group.cc
@@ -2,35 +2,18 @@
 #include "list_node.h"
 
 class ReverseNodesInKGroup {
-public:
     ListNode *reverseKGroup(ListNode *head, int k) {
         ListNode *token = new ListNode(0);
         token->next = head;
         head = token;
-<<<<<<< HEAD
-        ListNode *tail = head;
-        for ( ; ; ) {
-=======
         for ( ; ; ) {
             ListNode *tail = head;
->>>>>>> cee0291085b1fc5c266c9f29c21db92032414aa3
             for (int i = 0; i < k && tail != NULL; ++i) {
                 tail = tail->next;
             }
             if (NULL == tail) {
                 break;
             }
-<<<<<<< HEAD
-            ListNode *prev = head;
-            head = head->next;
-            ListNode *post = tail->next;
-            ListNode *rev_node = NULL;
-            for (ListNode *ptr = head; ptr != post; ) {
-            }
-        }
-    }
-};
-=======
             ListNode *post = tail->next;
             ListNode *prev = NULL;
             for (ListNode *ptr = head->next; ptr != post; ) {
@@ -53,4 +36,3 @@ public:
 int main(int argc, char *argv[]) {
     return 0;
 }
->>>>>>> cee0291085b1fc5c266c9f29c21db92032414aa3
